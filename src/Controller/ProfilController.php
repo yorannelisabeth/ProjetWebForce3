ff5ba1efabe3ac6b442ce75d30378eeb6c1a25ba
yorann <yorannelisabeth@gmail.com>
2021-05-25 15:57:54 +0200

    première gestion des accès au menu

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil" , name="profil")
-     
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
     
      */
     
