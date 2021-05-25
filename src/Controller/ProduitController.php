4f802a17e275fbe4c65adc08a9b7b8c81e3d93fe
morel <mchab2017@gmail.com>
2021-05-25 15:19:47 +0200

    menu à jour

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/' ,name: 'produit', methods: ['GET'])]
+    #[Route('/' ,name: 'produit_index', methods: ['GET'])]
     public function index(ProduitRepository $produitRepository): Response
     {
         return $this->render('produit/index.html.twig', [
