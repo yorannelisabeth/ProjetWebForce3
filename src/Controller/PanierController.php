b96ada8c61edfa3db1279bcc05c43acc73b72b2b
morel <mchab2017@gmail.com>
2021-05-31 16:47:45 +0200

    icones indexProduits

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -14,82 +14,80 @@ class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'panier_index')]
 
-    public function index( SessionInterface $session, ProduitRepository $produitRepository){
+    public function index(SessionInterface $session, ProduitRepository $produitRepository)
+    {
 
-    
-        $panier = $session -> get('panier', []); 
-        // $voirpanier = dd($panier);
 
-        // $panierProduits = [];
+        $panier = $session->get('panier', []);
+        
 
-        // foreach($panier as $id => $quantite){
+        $panierProduits = [];
 
-        //     $panierProduits[] = [
-        //         'produit' => $produitRepository -> find($id),
+        foreach ($panier as $id => $quantité) {
 
-        //         'quantite' => $quantite
-        //     ];
-        // }
-        // $total = 0 ;
-
-        // foreach ($panierProduits as $item){
-        //     $totalItem = $item['produit']->getprix() * $item['quantite'];
-        //     $total += $totalItem;
-        // }
-
-        return $this->render('panier/index.html.twig', [
-           
-
-            'panier' => $panier ,
-            'liste_categories'=>$produitRepository->listecategorie('$categorie'),
+            $panierProduits[] = [
+                'produit' => $produitRepository->find($id),
+                'quantité' => $quantité
+            ];
+        }
+            $total = 0;
             
-            // 'total' => $total
+            //$user = $this->session->getUser();
+
+            foreach ($panierProduits as $item) {
+            $totalItem = $item['produit']->getprix() * $item['quantité'];
+            $total += $totalItem;
+        }
+
+        
+            return $this->render('panier/index.html.twig', [
+
+
+            'items' => $panierProduits,
+            'panier' => $panier,
+            'liste_categories' => $produitRepository->listecategorie('$categorie'),
+
+             'total' => $total
         ]);
+       
     }
 
     #[Route('/ajout/{id}', name: 'ajout_produit')]
 
-    public function ajoutProduit($id,$nom ,SessionInterface $session ) {
+    public function ajoutProduit($id, SessionInterface $session)
+    {
         //sessionInterface qui est obtenu par le conteneur de servive par la commande 
         //symfony console debug:autowiring session
 
 
-        $panier = $session -> get('panier', []); // Récupérer un panier dans ma session et si je n'en ai pas j'en recup 
+        $panier = $session->get('panier', []); // Récupérer un panier dans ma session et si je n'en ai pas j'en recup 
         // un qui est un tableau d'où les crochets.
 
-        if(!empty($panier[$id])){
+        if (!empty($panier[$id])) {
 
             $panier[$id]++;
-            $panier[$nom]++;
+            
+        } else {
+            $panier[$id] = 1; // l'id qui respresente l'id d'un produit du panier a laquelle j'attribue la valeur 1 
         }
-        else{
-            $panier [$id] = 1;// l'id qui respresente l'id d'un produit du panier a laquelle j'attribue la valeur 1 
-        }
-         // l'id qui respresente l'id d'un produit du panier a laquelle j'attribue la valeur 1 
+        // l'id qui respresente l'id d'un produit du panier a laquelle j'attribue la valeur 1 
 
-        $session -> set('panier', $panier);
+        $session->set('panier', $panier);
 
-    return $this->redirectToRoute("panier_index");
-
-
+        return $this->redirectToRoute("panier_index");
     }
 
     #[Route('/remove/{id}', name: 'remove_produit')]
 
-    public function remove ($id, SessionInterface $session){
-        $panier = $session-> get('panier', []);
+    public function remove($id, SessionInterface $session)
+    {
+        $panier = $session->get('panier', []);
 
-        if(!empty($panier[$id])){
+        if (!empty($panier[$id])) {
             unset($panier[$id]);
         }
         $session->set('panier', $panier);
 
-    return $this->redirectToRoute("panier_index");
+        return $this->redirectToRoute("panier_index");
     }
-
-
-}   
-
-
-
-
+}
